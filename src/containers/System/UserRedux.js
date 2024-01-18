6eecd54e79a13908bd3cbceb9bfd514cbe88958c
TrH203 <hoangtrhien203@gmail.com>
2024-01-18 23:21:47 +0700

    update user-redux

--- src/containers/System/UserRedux.js
+++ src/containers/System/UserRedux.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
+import { Button } from 'reactstrap';
+import "./UserRedux.scss"
 import { connect } from 'react-redux';
 class UserRedux extends Component {
     constructor(props) {
@@ -17,10 +19,47 @@ class UserRedux extends Component {
             <>
                 <div className="user-redux-container" >
                     <div className='title'>User Redux from TrHien203</div>
-                    <div className='user-redux-body'>
-                        <div>Thêm mới người dùng</div>
+                    <div className='form-container'>
+                        <div className='container'>
+                            <form className="row g-3">
+                                <div className='col-md-3'>
+                                    <label for="inputEmail3"><FormattedMessage id='redux.email' /></label>
+                                    <input type="email" className="form-control" id="inputEmail3" name="email"  ></input>
+                                </div>
+                                <div className='col-md-3'>
+                                    <label for="inputPassword3"><FormattedMessage id='redux.password' /></label>
+                                    <input type="password" className="form-control" id="inputPassword3" name="password"></input>
+                                </div>
+                                <div className='col-md-3'>
+                                    <label for="inputText13"><FormattedMessage id='redux.firstName' /></label>
+                                    <input type="text" className="form-control" id="inputText13" name="password"></input>
+                                </div>
+                                <div className='col-md-3'>
+                                    <label for="inputText23"><FormattedMessage id='redux.lastName' /></label>
+                                    <input type="text" className="form-control" id="inputText23" name="password"></input>
+                                </div>
+
+                                <div className='col-md-3'>
+                                    <label for="inputEmail3"><FormattedMessage id='redux.email' /></label>
+                                    <input type="email" className="form-control" id="inputEmail3" name="email"  ></input>
+
+                                </div>
+                                <div className='col-md-3'>
+                                    <label for="inputPassword3"><FormattedMessage id='redux.password' /></label>
+                                    <input type="password" className="form-control" id="inputPassword3" name="password"></input>
+                                </div>
+                                <div className='col-md-3'>
+                                    <label for="inputText13"><FormattedMessage id='redux.firstName' /></label>
+                                    <input type="text" className="form-control" id="inputText13" name="password"></input>
+                                </div>
+                                <div className='col-md-3'>
+                                    <label for="inputText23"><FormattedMessage id='redux.lastName' /></label>
+                                    <input type="text" className="form-control" id="inputText23" name="password"></input>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                </div>
+                </div >
             </>
 
         )
